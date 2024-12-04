dcb70d76972de228c2c48a9d7171583d41519614
ADS 2022.2N <36124872022.2N@senacrjedu.local>
2024-12-03 21:30:01 -0300

    api properties

--- src/interface/seguro.ts
+++ src/interface/seguro.ts
@@ -1,14 +1,17 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class Seguro {
     
-    
+    @ApiProperty()
     seguro_id: number;
 
-
+    @ApiProperty()
     modelo: string
 
-
+    @ApiProperty()
     placa: string
-
+    
+    @ApiProperty()
     cliente_id: number
 
 }
